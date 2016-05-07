67f579f4f48a4248169a64b49dbcb94f52ba87e4
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2016-05-08 00:19:56 +0400

    Created the list with some restaurants

--- app/src/main/java/com/jiahaoliuliu/tomazo/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/tomazo/MainActivity.java
@@ -1,13 +1,42 @@
 package com.jiahaoliuliu.tomazo;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // List of restaurants
+    private String[] mRestaurants = {"My fancy restaurant 1", "Avec...", "Vapiano", "Khan Chacha"};
+
+    private ListView mRestaurantsList;
+
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = this;
+
+        mRestaurantsList = (ListView) findViewById(R.id.restaurants_list);
+
+        // Defines a new adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, mRestaurants);
+
+        mRestaurantsList.setAdapter(adapter);
+        mRestaurantsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent startRestaurantDetailsIntent = new Intent(mContext, RestaurantDetailsActivity.class);
+                startActivity(startRestaurantDetailsIntent);
+            }
+        });
     }
 }
