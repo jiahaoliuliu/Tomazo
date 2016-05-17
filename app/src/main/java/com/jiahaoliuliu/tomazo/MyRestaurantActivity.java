4e9e87c2dd0ba9a92a0e84fbd2830f6e85e392ca
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2016-05-18 00:15:24 +0400

    Merged with exerciseI

--- app/src/main/java/com/jiahaoliuliu/tomazo/MyRestaurantActivity.java
+++ app/src/main/java/com/jiahaoliuliu/tomazo/MyRestaurantActivity.java
@@ -1,43 +1,30 @@
 package com.jiahaoliuliu.tomazo;
 
-import android.content.Context;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MyRestaurantActivity extends AppCompatActivity {
 
-    // List of restaurants
-    private String[] mRestaurants = {"The big circle", "Al Soor", "Avec...", "Vapiano", "Khan Chacha", "Abu Shagara", "Nayaab Haandi", "Rajasthan Al Malaki",
-                                    "Grub Shack", "Orient grill", "Makani Restuarant"};
-
-    private ListView mRestaurantsList;
-
-    private Context mContext;
+    // Views
+    private TextView mTitle;
+    private ImageView mImage;
+    private TextView mDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_restaurant);
 
-        mContext = this;
+        // Link the view
+        mTitle = (TextView) findViewById(R.id.title);
+        mImage = (ImageView) findViewById(R.id.image);
+        mDescription = (TextView) findViewById(R.id.description);
 
-        mRestaurantsList = (ListView) findViewById(R.id.restaurants_list);
-
-        // Defines a new adapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, mRestaurants);
-
-        mRestaurantsList.setAdapter(adapter);
-        mRestaurantsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent startRestaurantDetailsIntent = new Intent(mContext, RestaurantDetailsActivity.class);
-                startActivity(startRestaurantDetailsIntent);
-            }
-        });
+        // Change the values
+        mTitle.setText("The fish hut");
+        mImage.setImageDrawable(getResources().getDrawable(R.drawable.fish_hut));
+        mDescription.setText("The fish hut is a really good restaurant");
     }
 }
