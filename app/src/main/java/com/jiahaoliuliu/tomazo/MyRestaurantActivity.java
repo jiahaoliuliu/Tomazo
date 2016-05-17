e72e7bf5068788c276ec2f70c8e60c3a65fb9596
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2016-05-18 00:08:36 +0400

    Renamed MainActivity to MyRestaurantActivity

--- app/src/main/java/com/jiahaoliuliu/tomazo/MyRestaurantActivity.java
+++ app/src/main/java/com/jiahaoliuliu/tomazo/MyRestaurantActivity.java
@@ -3,7 +3,7 @@ package com.jiahaoliuliu.tomazo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MyRestaurantActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
