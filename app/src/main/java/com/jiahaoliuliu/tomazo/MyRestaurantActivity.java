2e536f956f61d37ab172000c776aeec201f83d3a
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2016-05-18 00:19:02 +0400

    Added the header and the back button

--- app/src/main/java/com/jiahaoliuliu/tomazo/MyRestaurantActivity.java
+++ app/src/main/java/com/jiahaoliuliu/tomazo/MyRestaurantActivity.java
@@ -2,6 +2,7 @@ package com.jiahaoliuliu.tomazo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,6 +18,10 @@ public class MyRestaurantActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_restaurant);
 
+        // Displaying the back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
         // Link the view
         mTitle = (TextView) findViewById(R.id.title);
         mImage = (ImageView) findViewById(R.id.image);
@@ -26,5 +31,16 @@ public class MyRestaurantActivity extends AppCompatActivity {
         mTitle.setText("The fish hut");
         mImage.setImageDrawable(getResources().getDrawable(R.drawable.fish_hut));
         mDescription.setText("The fish hut is a really good restaurant");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Capture the back action
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
